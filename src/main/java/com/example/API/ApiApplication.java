fd4cb217f6f28463af7728b03e29bd1e150a82aa
TheRobinGuy <gilmartin.glenn@gmail.com>
2018-11-10 22:39:28 +0000

    Application properties updated, H2 DB working

--- src/main/java/com/example/API/ApiApplication.java
+++ src/main/java/com/example/API/ApiApplication.java
@@ -2,8 +2,15 @@ package com.example.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableJpaRepositories
+@EnableSwagger2
 @SpringBootApplication
+@EntityScan(basePackageClasses = { ApiApplication.class, Jsr310JpaConverters.class })
 public class ApiApplication {
 
 	public static void main(String[] args) {
